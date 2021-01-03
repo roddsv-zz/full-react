c54512c026d9c6c4b8987728efc719c82e4f6435
Rodrigo Soares <adsrodsoares@gmail.com>
2021-01-03 16:32:45 -0300

    Creating, exporting and importing Components

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import './index.css'
-import ReactDOM from 'react-dom'
-import React from 'react'
+import "./index.css";
+import ReactDOM from "react-dom";
+import React from "react";
+import First from "../src/components/basics/First";
 
 /*Importando o index.css (cuidado com o ./ por conta da pasta onde o mesmo se encontra.*/
-  
-const tag = <strong>Olá React!</strong>
 
 ReactDOM.render(
-   <div>
-     { tag }
-    </div>,
-    document.getElementById('root')
-)
+  <div>
+    <First />
+  </div>,
+  document.getElementById("root")
+
+  /*Seguir a sintaxe para referenciar o Component como uma tag <Nome_do_Component /> ou <Nome_do_Component></Nome_do_Component>
+  dentro do ReactDOM.render.*/
+);
