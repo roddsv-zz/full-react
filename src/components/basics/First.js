822f5932fb168123aa6b2d153bfd0ac2ce2b4e32
Rodrigo Soares <adsrodsoares@gmail.com>
2021-01-03 16:38:40 -0300

    Using tag into Component

--- src/components/basics/First.js
+++ src/components/basics/First.js
@@ -1,3 +1,11 @@
+import React from 'react'
+
 export default function First() {
-    return 'First Component'
+    const message = 'Seja bem-vindo!'
+    return (
+        <div>
+            <h2>First Component</h2>
+            <p>{ message }</p>
+        </div>
+    )
 }
